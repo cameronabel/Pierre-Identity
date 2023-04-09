07c1971a4c14a6f8c06b95c9a30aa0c507167908
Cameron Abel <cameronabel@gmail.com>
2023-04-09 06:06:03 -0700

    add basic model structure

---
+++ Bakery/Models/Flavor.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bakery.Models;
+
+public class Flavor
+{
+  public int FlavorId { get; set; }
+  [Required(ErrorMessage = "The flavor's name can't be empty")]
+  public string Name { get; set; }
+  public List<FlavorTreat> JoinEntities { get; }
+}
---
+++ Bakery/Models/Treat.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bakery.Models;
+
+public class Treat
+{
+  public int TreatId { get; set; }
+  [Required(ErrorMessage = "The treat's name can't be empty")]
+  public string Name { get; set; }
+  public List<FlavorTreat> JoinEntities { get; }
+}
